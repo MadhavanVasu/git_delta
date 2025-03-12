77e3504a153db92dea2631cc64324561d4d739e0
Madhavan V <madhavan@Madhavans-MacBook-Pro.local>
2025-03-12 10:34:56 +0530

    Added Hello world controller

---
+++ src/main/java/com/mv/git_delta/GitDeltaApplication.java
@@ -0,0 +1,13 @@
+package com.mv.git_delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDeltaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDeltaApplication.class, args);
+	}
+
+}
