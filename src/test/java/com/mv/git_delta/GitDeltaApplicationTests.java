3b550909aaea39735f4b54654718343c748e97e8
Madhavan V <madhavan@Madhavans-MacBook-Pro.local>
2025-03-12 10:38:36 +0530

    Added a simple spring boot app

---
+++ src/test/java/com/mv/git_delta/GitDeltaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mv.git_delta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDeltaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
